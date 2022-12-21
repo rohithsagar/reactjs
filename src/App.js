391ad409f14e8dfa217942034e2766c67bf4828d
rohith sagar <rohithsagars540@gmail.com>
2022-12-21 12:55:43 +0530

    state added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+
+state = {
+  myString : "Hello"
+};
+
   render() {
     return (
       <div className="App">
-        <h1>Welcome</h1>
+        <h1>{this.state.myString}</h1>
       </div>
     );
   }
